cf78add98aab7c8b9bd8205154c148a89da51c0c
Yanda Huang <ward11sdo@qq.com>
2018-03-25 00:50:25 -0400

    Fix multiple rendering error

--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import nipplejs from 'nipplejs';
 
 class JoyStick extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            rendered: false
+        }
+    }
     componentDidMount() {
         this.props.managerFn(this.manager)
     }
     render() {
         var renderJoy = function (element) {
-            const combinedOptions = Object.assign({ zone: element }, this.props.joyOptions)
-            this.manager = nipplejs.create(combinedOptions);
+            if (!this.state.rendered){
+                const combinedOptions = Object.assign({ zone: element }, this.props.joyOptions);
+                this.manager = nipplejs.create(combinedOptions);
+                this.setState({
+                    rendered: true
+                });
+            }
         }
         renderJoy = renderJoy.bind(this);
 
